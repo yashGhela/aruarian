215c587e13ffe9b07f6549ff4fc0276f9d423938
yashGhela <ghelayash@gmail.com>
2024-03-29 22:46:53 +0200

    updates pls work

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseURL =  process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-const supabaseAdminKey = process.env.NEXT_PUBLIC_SERVICE_ROLE_KEY
+
 
 
 export const supabase = createClient(supabaseURL, supabaseKey)
