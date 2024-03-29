8623d7c33aa6b0118b2940bc3f6761176bf65bb6
yashGhela <ghelayash@gmail.com>
2024-03-29 15:54:01 +0200

    fixed UI for mobile

--- app/Dashboard/page.jsx
+++ app/Dashboard/page.jsx
@@ -99,9 +99,9 @@ import { motion } from "framer-motion"
         console.log(snap)
         setIsLoading(false)
 
-        setTimeout(() => {
-          setMesSent(false);
-      }, 5000);
+      //   setTimeout(() => {
+      //     setMesSent(false);
+      // }, 5000);
       })
     
 
@@ -212,12 +212,12 @@ import { motion } from "framer-motion"
         
     {mesSent ?
 
-     <div className="mt-[20%] ml-[40%] w-[300px] h-self  backdrop-blur-sm    rounded-[40px] bg-white/20 border p-3  border-white/[0.06]">
+     <div className="md:mt-[20%] md:ml-[40%] mt-[90%] ml-10 w-[300px] h-self  backdrop-blur-sm    rounded-[40px] bg-white/20 border p-3  border-white/[0.06]">
 
        {IsLoading ?
 
         <div>
-        <div role="status" className="align-items-center ml-[45%] mt-[20%]">
+        <div role="status" className="align-items-center ml-[45%] ">
                 <svg aria-hidden="true" class="w-8 h-8  animate-spin dark:text-white fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
                     <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
@@ -229,7 +229,7 @@ import { motion } from "framer-motion"
        :
       <div>
 
-    <p className="text-center text-md font-normal mt-2 text-white">{responseAI.message}</p>
+    <p className="text-center text-md font-normal mt-2 p-3 text-white">{responseAI.message}</p>
 
     <button onClick={()=>{setMesSent(false)}} className=' text-white p-4 mt-14 w-full rounded-2xl bg-white/20 border font-bold  float-left  border-white/[0.06] '> Thanks!</button>
         
@@ -300,7 +300,7 @@ import { motion } from "framer-motion"
      <motion.div className=" fixed flex mt-[90%] w-full bottom-8" initial={{ opacity: 0 }}
     whileInView={{ opacity: 1 }}
     viewport={{ once: false  }}>
-     <input onChange={(e)=>{setPrompt(e.target.value)}} type="text"  className=" md:ml-[22%] lg:ml-[22%] 2xl:ml-[6%] xl:ml-[12%] ml-12 w-[75%] md:w-1/2 focus:outline-none p-2 rounded-lg bg-white/20 border  backdrop-blur-sm   border-white/[0.06]" />
+     <input onChange={(e)=>{setPrompt(e.target.value)}} type="text"  className=" md:ml-[22%] lg:ml-[22%] 2xl:ml-[6%] xl:ml-[12%] ml-5 w-[75%] md:w-1/2 focus:outline-none p-2 rounded-lg bg-white/20 border  backdrop-blur-sm   border-white/[0.06]" />
      <motion.button onClick={()=>{sendPrompt(); setMesSent(true); setIsLoading(true)}} whileHover={{scale:1.02}} className=" ml-2  w-10 focus:outline-none p-2 rounded-lg bg-white/20 border hover:bg-white/30   border-white/[0.06]" ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
   <path d="M3.478 2.404a.75.75 0 0 0-.926.941l2.432 7.905H13.5a.75.75 0 0 1 0 1.5H4.984l-2.432 7.905a.75.75 0 0 0 .926.94 60.519 60.519 0 0 0 18.445-8.986.75.75 0 0 0 0-1.218A60.517 60.517 0 0 0 3.478 2.404Z" />
 </svg>
