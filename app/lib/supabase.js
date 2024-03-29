638b2965a54daa9db052062adf13859653b4c913
yashGhela <ghelayash@gmail.com>
2024-03-29 22:35:41 +0200

    prod chaanges

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 
-const supabaseURL =  process.env.SUPABASE_URL 
-const supabaseKey = process.env.SUPABASE_ANON_KEY
-const supabaseAdminKey = process.env.SERVICE_ROLE_KEY
+const supabaseURL =  process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseAdminKey = process.env.NEXT_PUBLIC_SERVICE_ROLE_KEY
 
 
 export const supabase = createClient(supabaseURL, supabaseKey)
