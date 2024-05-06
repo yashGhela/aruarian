4e3d04f82a536adb2a0c1c3d989d3991519b55cf
yashGhela <ghelayash@gmail.com>
2024-05-06 17:31:06 +0200

    Holy shit i dont know how to code

--- app/Dashboard/page.jsx
+++ app/Dashboard/page.jsx
@@ -298,10 +298,10 @@ const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
            <title>Dashboard</title>
          </header>
          <main 
-         className=' flex text-neutral-900 bg-gradient-to-b from-slate-300 via-red-100 to-orange-300 bg-cover self-center place-content-center  backdrop-blur-md     text-center p-5 max-w-full min-w-screen  min-h-screen max-h-full overflow-hidden'>
+         className=' flex text-neutral-900 bg-gradient-to-b from-purple-100 via bg-amber-100 to-orange-200 bg-cover self-center place-content-center  backdrop-blur-md     text-center p-5 max-w-full min-w-screen  min-h-screen max-h-full overflow-hidden'>
      
      <Overlay boards={boards} setBoardQuery={setBoardQuery} setQueried={setQueried} />
-     <div className="fixed inset-0   bg-black opacity-10"></div>
+     <div className="fixed inset-0   bg-black opacity-5"></div>
 
    
 
@@ -382,9 +382,9 @@ const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
      </div>:
      
-     <div className=" md:ml-40  lg:ml-40 2xl:ml-0" id="first open ">
+     <div className="    mt-[10%]  " id="first open ">
 
-     <div className="flex mt-[20%] mx-20">
+     {/* <div className="flex mt-[10%] mx-20">
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4  mt-[2px] font-thin ">
 <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
 </svg>
@@ -395,17 +395,17 @@ const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 <path strokeLinecap="round" strokeLinejoin="round" d="m6.115 5.19.319 1.913A6 6 0 0 0 8.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 0 0 2.288-4.042 1.087 1.087 0 0 0-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 0 1-.98-.314l-.295-.295a1.125 1.125 0 0 1 0-1.591l.13-.132a1.125 1.125 0 0 1 1.3-.21l.603.302a.809.809 0 0 0 1.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 0 0 1.528-1.732l.146-.292M6.115 5.19A9 9 0 1 0 17.18 4.64M6.115 5.19A8.965 8.965 0 0 1 12 3c1.929 0 3.716.607 5.18 1.64" />
 </svg>
 
-<p className=' text-sm text-left ml-2  font-thin'>Later</p> */}
+<p className=' text-sm text-left ml-2  font-thin'>Later</p>
 
 
-   </div>
+   </div> */}
    
-  <div className="mt-8 mx-20 xs:flex-col lg:flex  xl:flex">
+  <div className="sm:mt-8  mt-32 ">
    
-  <p className=' text-4xl text-left  font-normal'>{greeting}, <br/> You have  <span className="font-bold">{todos.length} To-Dos </span> left for <br/> the day</p>
+  <p className=' text-4xl   font-normal'>{greeting}, You have  <span className="font-bold">{todos.length} To-Dos </span> left for <br/> the day</p>
 
 
-  <div className="lg:ml-[20%] md:w-[50%] -mt-10" id="to-dos container ">
+  <div className=" 2xl:mx-[25%] lg:mx-64 md:mx-24  xl:mx-[25%] sm:mx-5 mt-10" id="to-dos container ">
 
   <Container isRes={false} setTodos={setTodos} boards={boards} todos={todos} todayEnd={todayEnd} todayStart={todayStart} />
 
@@ -438,19 +438,19 @@ const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
       )} */}
 
      
-     <motion.div className=" fixed flex-col mt-[90%] w-full bottom-8" initial={{ opacity: 0 }}
+     <motion.div className=" md:ml-32 lg:ml-64  xl:ml-72 2xl:ml-32 fixed flex-col mt-[90%] w-full bottom-8" initial={{ opacity: 0 }}
     whileInView={{ opacity: 1 }}
     viewport={{ once: false  }}>
-    <div className="flex md:visible invisible mb-2 md:ml-40 2xl:ml-0">
-    <motion.button onClick={()=>{setPrompt(' What work do I have for today?'); }} whileHover={{scale:1.03}} className='  ml-[7.2rem] w-1/6 focus:outline-none p-2 rounded-lg shadow-sm bg-white/40   hover:bg-white/50 border border-white/[0.06]  backdrop-blur-sm   "'>What work do I have for today? </motion.button>
-    <motion.button onClick={()=>{setPrompt(' What tasks are overdue?'); }} whileHover={{scale:1.03}} className='  ml-2 w-1/6 focus:outline-none p-2 rounded-lg bg-white/40 shadow-sm   hover:bg-white/50 border border-white/[0.06]  backdrop-blur-sm   "'>What tasks are overdue? </motion.button>
-    <motion.button onClick={()=>{setPrompt(' Whats up for tomorrow?');}} whileHover={{scale:1.03}} className='  ml-2 w-1/6 focus:outline-none p-2 rounded-lg bg-white/40  shadow-sm  hover:bg-white/50 border border-white/[0.06]  backdrop-blur-sm   "'>Whats up for tomorrow? </motion.button>
+    <div className="flex md:visible invisible mb-2 md:ml-0  xl:ml-0">
+    <motion.button onClick={()=>{setPrompt(' What work do I have for today?'); }} whileHover={{scale:1.03}} className='  w-1/6 focus:outline-none p-2 rounded-lg shadow-sm bg-white/40   hover:bg-white/50 border border-white/[0.06]  backdrop-blur-sm   "'>What work do I have for today? </motion.button>
+    <motion.button onClick={()=>{setPrompt(' What tasks are overdue?'); }} whileHover={{scale:1.03}} className=' ml-2  w-1/6 focus:outline-none p-2 rounded-lg bg-white/40 shadow-sm   hover:bg-white/50 border border-white/[0.06]  backdrop-blur-sm   "'>What tasks are overdue? </motion.button>
+    <motion.button onClick={()=>{setPrompt(' Whats up for tomorrow?');}} whileHover={{scale:1.03}} className='ml-2  w-1/6 focus:outline-none p-2 rounded-lg bg-white/40  shadow-sm  hover:bg-white/50 border border-white/[0.06]  backdrop-blur-sm   "'>Whats up for tomorrow? </motion.button>
 
 
     </div>
     <div className="flex">
    
-       <input value={prompt}   onChange={(e)=>{setPrompt(e.target.value)}} type="text"  className=" md:ml-[22%] lg:ml-[22%] 2xl:ml-[6%] xl:ml-[12%] ml-5 w-[75%] md:w-1/2 focus:outline-none p-2 rounded-lg bg-white/40 border  backdrop-blur-sm   border-white/[0.06]" />
+       <input value={prompt}   onChange={(e)=>{setPrompt(e.target.value)}} type="text"  className="  md:w-1/2 w-[80%] focus:outline-none p-2 rounded-lg bg-white/40 border  backdrop-blur-sm   border-white/[0.06]" />
      <motion.button onClick={()=>{sendPrompt(); setMesSent(true); setIsLoading(true)}} whileHover={{scale:1.02}} className=" ml-2  w-10 focus:outline-none p-2 rounded-lg bg-white/40 border hover:bg-white/50  text-white  border-white/[0.06]" ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">  <path d="M3.478 2.404a.75.75 0 0 0-.926.941l2.432 7.905H13.5a.75.75 0 0 1 0 1.5H4.984l-2.432 7.905a.75.75 0 0 0 .926.94 60.519 60.519 0 0 0 18.445-8.986.75.75 0 0 0 0-1.218A60.517 60.517 0 0 0 3.478 2.404Z" /></svg> </motion.button>
      
     </div>
