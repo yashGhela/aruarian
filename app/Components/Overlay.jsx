131a370bf514d801f0b2df2bcc605a14e0f4bb01
yashGhela <ghelayash@gmail.com>
2024-05-06 17:46:39 +0200

    ok theres a bit more vision now with UI

--- app/Components/Overlay.jsx
+++ app/Components/Overlay.jsx
@@ -77,7 +77,7 @@ function Overlay({boards, setQueried, setBoardQuery}) {
         <motion.div whileHover={{scale:1.05}}    className={`flex z-20 fixed cursor-pointer hover:shadow-inner  top-8 left-8 rounded-full p-1 `}>
 
         <svg
-  className='w-8 h-10 text-white/50'
+  className='w-8 h-10 text-black/80'
   viewBox="0 0 57 79"
   fill="none"
   xmlns="http://www.w3.org/2000/svg"
