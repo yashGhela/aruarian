7b215addb77626035a8ae9651d21e96b05418cce
yashGhela <ghelayash@gmail.com>
2024-05-09 14:52:19 +0200

    Fixed the input string being too long

--- app/Dashboard/page.jsx
+++ app/Dashboard/page.jsx
@@ -298,8 +298,8 @@ const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
            <title>Dashboard</title>
          </header>
          <main 
-         style={{background: 'linear-gradient(0deg, rgba(219,200,175,1) 0%, rgba(226,169,169,1) 29%, rgba(255,164,93,1) 44%, rgba(255,233,233,1) 74%);'}}
-         className=' flex text-neutral-900  bg-cover self-center place-content-center  backdrop-blur-md     text-center p-5 max-w-full min-w-screen  min-h-screen max-h-full overflow-hidden'>
+         style={{backgroundImage: 'url(https://images.unsplash.com/photo-1484353623933-a859263673a9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)'}}
+         className=' flex text-neutral-900  bg-fill self-center place-content-center  backdrop-blur-md     text-center p-5 max-w-full min-w-screen  min-h-screen max-h-full overflow-hidden'>
      
      <Overlay boards={boards} setBoardQuery={setBoardQuery} setQueried={setQueried} />
      {/* <div className="fixed inset-0   bg-black opacity-10"></div> */}
