bce271b75e5ec91c70769d82073437d8efff9a34
yashGhela <ghelayash@gmail.com>
2024-05-14 20:53:49 +0200

    fixed the pricing thingy

--- app/Components/Pricing.jsx
+++ app/Components/Pricing.jsx
@@ -34,7 +34,7 @@ export default function Pricing({landing}){
 
     return(
 
-        <div className={`${landing?"mt-[140%] md:mt-[60%] lg:mt-[40%] sm:mt-[150%]":"mt-10"} flex-col`} >
+        <div className={`${landing?"mt-[60%] md:mt-[60%] lg:mt-[40%] sm:mt-[150%]":"mt-10"} flex-col`} >
 
 <p className=" text-8xl text-center font-bold">Pricing</p>
             {/* <button onClick={()=>{setOpenModal(true)}} className="p-2 bg-white/40 hover:bg-white/50 border border-white/[0.13] rounded-lg  mt-4">Why do I need to pay for this?</button> */}
@@ -59,13 +59,13 @@ export default function Pricing({landing}){
                 
             </Modal>
 
-            <div className="lg:grid lg:grid-cols-2 md:mx-[30%] lg:mx-0 ml-2    flex-col mt-10">
+            <div className="lg:grid lg:grid-cols-2 md:mx-[30%] lg:mx-0 ml-2    flex-col mt-20">
 
                
 
                 
 
-            <div className="sm:w-[25rem] w-[23rem] mx-2 md:mt-10 mt-10 lg:mt-0  lg:mx-[70%] xl:mx-[70%] bg-white/40 rounded-xl h-[23rem] shadow-2xl  border-2 border-white/[0.13] ">
+            <div className="sm:w-[25rem] w-[23rem] ml-20 md:mt-10 mt-10 lg:mt-0  lg:mx-[70%] xl:mx-[79%] bg-white/40 rounded-xl h-[23rem] shadow-2xl  border-2 border-white/[0.13] ">
 
                 
 
@@ -76,7 +76,7 @@ export default function Pricing({landing}){
                     
 
                     <p className='font-bold text-6xl text-centertext-neutral-700 mt-2 '>$5<span className='font-normal  text-xl  md:text-[30px] text-neutral-600 mt-2'>/month</span></p>
-                    <p className="mt-5 px-10">Early access for Aruarian for a lower price </p>
+                    <p className="mt-10 px-10">Early access for Aruarian for a lower price </p>
                     
                     <div className=" ml-20">
                         
