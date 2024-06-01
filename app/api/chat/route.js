1f5cc8c90fcc07baf1187d2195484b72a5c6342d
yashGhela <ghelayash@gmail.com>
2024-06-01 21:12:28 +0200

    uhHHhhhh

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -32,13 +32,13 @@ export async function POST(req, res) {
   const {data, error} = await supabase.from('To-Dos').select('*').eq('UID', body.userid).limit(50).order('due_date', {ascending:false})
 
 
-  const {paiddata,paiderror}= await supabase.from('Users').select('*').eq('UID', userid)
+  // const {paiddata,paiderror}= await supabase.from('Users').select('*').eq('UID', userid)
   
-      console.log(data)
-        if (paiddata[0].paid){
-          isPaid=true
+  //     console.log(data)
+  //       if (paiddata[0].paid){
+  //         isPaid=true
           
-        }
+  //       }
 
   
 
@@ -80,7 +80,7 @@ export async function POST(req, res) {
             " insert_response_format:{action: insert, responseMessage: Your response to the prompt(in relation to the content, eq: sure I'll remind you to get new shoes this weekend!), content: (must always have a string value [no objects just a sentence], do not contain dates here), due_date: (eg:2024-04-01 12:47:14 [DO NOT INCLUDE GMT+0200 (South Africa Standard Time),board:from prompt or simply General])}, read_steps: You are given a prompt and a datachunk, you must answer the users question and return either a list or a single item from the datachunk, JSON response format:{action:read, responseMessage: Your response to the prompt, taskOrder: An array of tasks (extracted from the data given to you[with  fields: tid, content, due_date,completed])}, you only return JSON in this format }}}, text_respone_format:{action: text, responseMessage:(your response to the prompt)}`
             +"Prompt:"+ body.prompt
             +`/////  Today is  ${today}`
-            +`paid status:${isPaid}`
+            
             +'////Data Chunk for reading: '
             +JSON.stringify(objArray)
             +'///userid:'+userid
