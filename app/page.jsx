cd21c4f588f822a8066babaa8fa49073605502bb
yashGhela <ghelayash@gmail.com>
2024-06-11 15:34:56 +0200

    fixed ga

--- app/page.jsx
+++ app/page.jsx
@@ -60,7 +60,7 @@ export default function Home() {
      <script async src="https://www.googletagmanager.com/gtag/js?id=G-D8HTEWKHSM"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  function gtag(){dataLayer.push(arguments)}
   gtag('js', new Date());
 
   gtag('config', 'G-D8HTEWKHSM');
